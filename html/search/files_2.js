03120d8e87143e3a0c5d3d29899f3bca4fa41ec7
Anastasia Fongrad <fongradnastya@gmail.com>
2023-04-27 08:45:18 +0700

    Added documentation

---
+++ html/search/files_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['signals_2eh_0',['signals.h',['../signals_8h.html',1,'']]],
+  ['socket_2dclient_2ec_1',['socket-client.c',['../socket-client_8c.html',1,'']]],
+  ['socket_2dserver_2ec_2',['socket-server.c',['../socket-server_8c.html',1,'']]]
+];
---
+++ html/search/functions_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['main_0',['main',['../socket-client_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;socket-client.c'],['../socket-server_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;socket-server.c']]],
+  ['matrixprocessing_1',['matrixProcessing',['../matrix_8h.html#aaf0bbb5de6083880f1e8f64885a8103f',1,'matrix.c']]]
+];
